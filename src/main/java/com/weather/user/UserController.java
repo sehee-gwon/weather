9c1fc517f398a7590d29000f83d8bcd6f8d657d1
sehee_gwon <gwonsehwi@gmail.com>
2021-08-22 21:20:26 +0900

    gitignore 적용 및 controller logger 적용

--- src/main/java/com/weather/user/UserController.java
+++ src/main/java/com/weather/user/UserController.java
@@ -1,5 +1,8 @@
 package com.weather.user;
 
+import com.weather.user.domain.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,19 +10,27 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import com.weather.user.domain.User;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/user")
 public class UserController {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private UserService userService;
 
     @GetMapping("/info")
     public String info(Model model) {
-        List<User> list = userService.getUserList();
+        List<User> list = null;
+
+        try {
+            list = userService.getUserList();
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+
         model.addAttribute("list", list);
         return "OK";
     }
