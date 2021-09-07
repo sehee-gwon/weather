6ee9b350ac973af3c88a35f2b5fd807d330827e4
TaeJa <hojunii95@gmail.com>
2021-09-08 02:02:35 +0900

    index2.html 추가 -> 위치정보 받지 못하였을때 기온 정보 미노출 되도록 v-if문 추가

--- src/main/java/com/weather/api/OpenWeatherApiController.java
+++ src/main/java/com/weather/api/OpenWeatherApiController.java
@@ -8,9 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URLEncoder;
