4e05a295ad7d646c87e329a1e05dfa17326c57ed
sehee gwon <gwonsehwi@gmail.com>
2021-09-24 00:37:34 +0900

    feat: 파일 경로 재배치, 사용하지 않는 파일 제거

--- src/main/resources/static/include/layout/header.vue
+++ src/main/resources/static/include/layout/header.vue
@@ -4,9 +4,10 @@
     <nav id="nav">
       <ul>
         <li><a href="/index.html">Home</a></li>
-        <li><a href="/generic.html">Generic</a></li>
+        <li><a href="/weather.html">Weather</a></li>
+        <li><a href="/board/list.html">Board</a></li>
         <li><a href="/elements.html">Elements</a></li>
-        <li><a href="#" class="button special">Sign Up</a></li>
+        <li><a href="/sign-up.html" class="button special">Sign Up</a></li>
       </ul>
     </nav>
   </header>
