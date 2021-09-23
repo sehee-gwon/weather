74af814e62eb2b8acabe9916bececb2ce73bbc06
TaeJa <hojunii95@gmail.com>
2021-09-23 23:48:39 +0900

    header.vue 추가 -> index.html의 header 부분을 따로 파일로 생성

---
+++ src/main/resources/static/include/header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1><a href="/index.html">Transit</a></h1>
+    <nav id="nav">
+      <ul>
+        <li><a href="/index.html">Home</a></li>
+        <li><a href="/generic.html">Generic</a></li>
+        <li><a href="/elements.html">Elements</a></li>
+        <li><a href="#" class="button special">Sign Up</a></li>
+      </ul>
+    </nav>
+  </div>
+</template>
