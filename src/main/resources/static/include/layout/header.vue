f46bbfd2689ae24f1abc2021aff60d315d8f5f2b
sehee gwon <gwonsehwi@gmail.com>
2021-09-24 00:38:56 +0900

    feat: index.html -> / 경로 수정

--- src/main/resources/static/include/layout/header.vue
+++ src/main/resources/static/include/layout/header.vue
@@ -1,9 +1,9 @@
 <template>
   <header id="header">
-    <h1><a href="/index.html">Transit</a></h1>
+    <h1><a href="/">Transit</a></h1>
     <nav id="nav">
       <ul>
-        <li><a href="/index.html">Home</a></li>
+        <li><a href="/">Home</a></li>
         <li><a href="/weather.html">Weather</a></li>
         <li><a href="/board/list.html">Board</a></li>
         <li><a href="/elements.html">Elements</a></li>
