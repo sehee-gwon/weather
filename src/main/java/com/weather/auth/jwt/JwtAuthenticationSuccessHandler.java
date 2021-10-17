09da364b9a3c3277fdd5ec4219777c4846b85583
sehee gwon <gwonsehwi@gmail.com>
2021-10-18 02:35:58 +0900

    feat: - 필요없는 코드 삭제 및 JwtFilter 컴포넌트 추가

--- src/main/java/com/weather/auth/jwt/JwtAuthenticationSuccessHandler.java
+++ src/main/java/com/weather/auth/jwt/JwtAuthenticationSuccessHandler.java
@@ -6,7 +6,6 @@ import com.weather.auth.domain.Auth;
 import com.weather.common.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import javax.servlet.http.HttpServletRequest;
--- src/main/java/com/weather/auth/jwt/JwtFilter.java
+++ src/main/java/com/weather/auth/jwt/JwtFilter.java
@@ -4,6 +4,7 @@ import com.weather.common.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -13,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@Component
 @RequiredArgsConstructor
 public class JwtFilter extends OncePerRequestFilter {
     private final JwtUtil jwtUtil;
